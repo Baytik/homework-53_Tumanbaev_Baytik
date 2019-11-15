f0b17fbfb449de86a918797072e572a883c77d3a
Baytik Tumanbaev <jumka.wows@gmail.com>
2019-11-15 18:04:59 +0600

    do all homework

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Task from "./component/Task/Task";
+import nanoid from 'nanoid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        tasks: [
+            {text: 'Buy Milk', id:nanoid()},
+            {text: 'Walk with dog', id:nanoid()},
+            {text: 'Do homework', id:nanoid()},
+        ],
+        task: ''
+    };
+
+    changeInput = (e) => this.setState({task: e.target.value});
+
+    addNewTask = () => {
+        const newTask = {
+            id: nanoid(),
+            text: this.state.task
+        };
+        this.setState({tasks: [...this.state.tasks, newTask]})
+    };
+
+    removeTasks = id => {
+        const findTasks = this.state.tasks.findIndex(r => r.id === id);
+        const tasks = [...this.state.tasks];
+        tasks.splice(findTasks, 1);
+        this.setState({tasks})
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <input onChange={this.changeInput}  type="text" placeholder="Add new task"/>
+                <button onClick={this.addNewTask} className="btn">Add</button>
+                {this.state.tasks.map(task =>
+                    <Task
+                        remove={() => this.removeTasks(task.id)}
+                        key={task.id}
+                        text={task.text}
+                    />
+                )}
+            </div>
+        )
+    }
 }
-
 export default App;
